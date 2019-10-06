ec7e837237c396e14f470075952ebae7c7c8f562
florpatrone <patroneflorenciacarla@gmail.com>
2019-10-06 14:18:23 -0300

    Creados y definidos structs 'campo' y 'hash'

--- hash.c
+++ hash.c
@@ -1,6 +1,19 @@
 #include "hash.h"
+#include "lista.h"
 #include <stdlib.h>
 #include <stdbool.h>
+
+typedef struct campo {
+    char* clave;
+    void* valor;
+} campo_t;
+
+struct hash {
+    lista_t** lista;
+    size_t capacidad;
+    size_t cantidad;
+    hash_destruir_dato_t* funcion_destruccion;
+};
 
 unsigned long funcion_hash(char *str, size_t cantidad) { //Utiliza el algoritmo 'djb2'
     unsigned long hash = 5381;

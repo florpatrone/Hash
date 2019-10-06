34956116da74680b8f024d977f9f9d3f4f904603
florpatrone <patroneflorenciacarla@gmail.com>
2019-10-06 12:15:08 -0300

    Agregada funcion de hash; utiliza el algoritmo djb2

--- hash.c
+++ hash.c
@@ -1,3 +1,13 @@
 #include "hash.h"
 #include <stdlib.h>
 #include <stdbool.h>
+
+unsigned long funcion_hash(char *str, size_t cantidad) { //Utiliza el algoritmo 'djb2'
+    unsigned long hash = 5381;
+    int c;
+    
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + (unsigned long)c; /* hash * 33 + c */
+    
+    return hash%cantidad;
+}
